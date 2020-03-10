a29ff871bd23f0d722cd578c55a0c8c9655d4968
Andrei <welldown44@gmail.com>
2020-03-10 19:40:43 +0200

    New options
    Add option to calculate the minimum amount // Add option to calculate the maximum amount

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -11,7 +11,23 @@ namespace ATM
 
             var users = new List<User>();
             users.Add(new User { username = "John", pin = "1234", balance = 100, surname = "ion", name = "faranume", phone = "0722993322" });
-            users.Add(new User { username = "Kevin", pin = "4321", balance = 200, surname = "john", name = "kevinache", phone = "0799123123" });
+            users.Add(new User { username = "Kevin", pin = "4321", balance = 1000, surname = "john", name = "kevinache", phone = "0799123123" });
+            users.Add(new User { username = "Mark", pin = "4321", balance = 300, surname = "john", name = "kevinache", phone = "0799123123" });
+            users.Add(new User { username = "Iosif", pin = "4321", balance = 800, surname = "john", name = "kevinache", phone = "0799123123" });
+            users.Add(new User { username = "Henry", pin = "4321", balance = 250, surname = "john", name = "kevinache", phone = "0799123123" });
+            users.Add(new User { username = "Yan", pin = "4321", balance = 600, surname = "john", name = "kevinache", phone = "0799123123" });
+            users.Add(new User { username = "Ming", pin = "4321", balance = 500, surname = "john", name = "kevinache", phone = "0799123123" });
+            users.Add(new User { username = "Marius", pin = "4321", balance = 50, surname = "john", name = "kevinache", phone = "0799123123" });
+            users.Add(new User { username = "Martin", pin = "4321", balance = 2500, surname = "john", name = "kevinache", phone = "0799123123" });
+            users.Add(new User { username = "Ionut", pin = "4321", balance = 500, surname = "john", name = "kevinache", phone = "0799123123" });
+            users.Add(new User { username = "Adrian", pin = "4321", balance = 300, surname = "john", name = "kevinache", phone = "0799123123" });
+
+            var minimumNumberToFind = users[0].balance;
+            var maximumNumberToFind = users[0].balance;
+            var sum = 0;
+            var totalAmount = 0;
+            var whoIsThis = string.Empty;
+
 
             //var pinCode = "1234";
             var selectedPin = string.Empty;
@@ -26,7 +42,7 @@ namespace ATM
                 Console.WriteLine("Login ---> 1");
                 Console.WriteLine("Exit ---> 0");
                 var input = Console.ReadLine();
-
+                
                 if(input == "0")
                 {
                     Console.WriteLine("Bye");
@@ -38,19 +54,22 @@ namespace ATM
                     {
                         Console.WriteLine("Please insert your username:");
                         enteredUsername = Console.ReadLine();
+
                         foreach (var user in users)
                         {
 
                             if (user.username == enteredUsername)
                             {
                                 userfound = true;
-                                var goBack = false;
                                 Console.WriteLine("\nPlease specify your pincode:");
                                 selectedPin = Console.ReadLine();
-                                    if (wrongPin == 3)
+                                    
+                                if (wrongPin == 2)
                                     {
                                         Console.WriteLine("You are banned from this ATM, muhahahahahaah ");
-                                        return;
+                                        input = "0";
+                                        mainMenu = true;    
+                                        break;
                                     }
                                     if (user.pin == selectedPin)
                                     {
@@ -61,12 +80,16 @@ namespace ATM
                                             Console.WriteLine($"\nHello, {user.username} ......");
                                             Console.WriteLine($"You have available the following amount: {user.balance}");
                                         } 
-                                        Console.WriteLine("\nIf you want to print out the user information ---> ENTER 1");
-                                        Console.WriteLine("Logout ---> ENTER 2");
-                                        Console.WriteLine("Exit ---> ENTER 3");
+                                    Console.WriteLine("\nIf you want to print out the user information ---> ENTER 1");
+                                    Console.WriteLine("See the minimum balance of your users ---> ENTER 2");
+                                    Console.WriteLine("See the maximum balance of your users ---> ENTER 3");
+                                    Console.WriteLine("Logout ---> ENTER 4");
+                                    Console.WriteLine("Exit ---> ENTER 5");
+
                                         var option = Console.ReadLine();
                                         if (option == "1")
                                         {
+                                            printMenu:
                                             Console.WriteLine($"\nSurname: {user.surname}");
                                             Console.WriteLine($"\nName: {user.name}");
                                             Console.WriteLine($"\nPhone: {user.phone}");
@@ -78,20 +101,69 @@ namespace ATM
                                             {
                                                 goto pageAfterLogin;
                                             }
+                                            else
+                                            {
+                                                goto printMenu;   
+                                            }
                                             
                                         }
                                         else if (option == "2")
                                         {
-                                            userIsLogged = false;
-                                            goBack = true;
-                                            Console.WriteLine("Logged out\n");
-                                            goto mainMenu;
+                                            foreach (var maxBalance in users)
+                                            {
+
+
+                                                if (maxBalance.balance <= minimumNumberToFind)
+                                                {
+                                                    minimumNumberToFind = maxBalance.balance;
+                                                    whoIsThis = maxBalance.username;
+                                                }
+                                            }
+                                        Console.WriteLine($"The minimum balance in you system is: {minimumNumberToFind} in {whoIsThis}'s account");
+                                        Console.WriteLine("------------------------------------------");
+                                        goto pageAfterLogin;
+                                        
                                         }
                                         else if (option == "3")
                                         {
+                                            foreach (var maxBalance in users)
+                                            {
+
+                                                totalAmount = maxBalance.balance + sum;
+                                                if (maxBalance.balance >= maximumNumberToFind)
+                                                {
+                                                    maximumNumberToFind = maxBalance.balance;
+                                                    whoIsThis = maxBalance.username;
+                                                }
+                                            }
+                                         Console.WriteLine($"The maximum balance in you system is: {maximumNumberToFind} in {whoIsThis}'s account");
+                                         Console.WriteLine("------------------------------------------");
+                                         goto pageAfterLogin;
+
+                                        
+                                        }
+                                       
+
+                                        else if(option == "4")
+                                        {
+                                            userIsLogged = false;
+
+                                            Console.WriteLine("Logged out\n");
+                                            goto mainMenu;
+
+                                        }
+                                        else if(option == "5")
+                                        {
+                                            input = "0";
+                                            mainMenu = true;
                                             break;
                                         }
-                                    }
+                                        else
+                                        {
+                                            goto pageAfterLogin;
+                                        }
+
+                                }
                                     else if (selectedPin != user.pin)
                                     {
                                         wrongPin++;

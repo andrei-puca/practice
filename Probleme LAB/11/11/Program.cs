ec9d7753e17d047496a2b72fcb99d522460ba5ed
Andrei <welldown44@gmail.com>
2020-04-14 18:09:30 +0300

    Problema 11 si 13
    Problema 11 si 13

---
+++ Probleme LAB/11/11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int givenNumber;
+            int counter = 0;
+
+            givenNumber = Convert.ToInt32(Console.ReadLine());
+
+            while(givenNumber > 0)
+            {
+                if ((givenNumber % 10) % 2 == 0)
+                    counter++;
+                givenNumber = givenNumber / 10;
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
---
+++ Probleme LAB/13/13/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber;
+            int secondNumber;
+            int thirdNumber;
+
+            firstNumber = Convert.ToInt32(Console.ReadLine());
+            secondNumber = Convert.ToInt32(Console.ReadLine());
+            thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (((firstNumber + secondNumber) > thirdNumber) && ((firstNumber + thirdNumber) > secondNumber) && ((thirdNumber + secondNumber) > firstNumber))
+                Console.WriteLine("This numbers can form a triangle");
+            else
+                Console.WriteLine("This numbers cannot form a triangle");
+        }
+    }
+}

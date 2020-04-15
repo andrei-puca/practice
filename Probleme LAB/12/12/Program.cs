6176c6449923dce780fa6c0ec39169a256c6b356
Andrei <welldown44@gmail.com>
2020-04-15 15:24:05 +0300

    problema12
    problema12

---
+++ Probleme LAB/12/12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long firstNumber;
+            long numberToReach;
+            long count = -1;
+
+            firstNumber = Convert.ToInt32(Console.ReadLine());
+            numberToReach = Convert.ToInt32(Console.ReadLine());
+
+            for (long power = 0; power <= numberToReach; power++)
+            {
+                if(  (long)Math.Pow(firstNumber, power) <= numberToReach ) {
+
+                    count++;
+                    
+                }
+                else
+                {
+                        break;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}

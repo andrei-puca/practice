173e67343721f14df5537c2a839686c9a4c62ee4
Andrei <welldown44@gmail.com>
2020-04-21 15:06:42 +0300

    lab1
    2.1

---
+++ Probleme LAB2/2.1/2.1/2.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int n;
+            int max = 0;
+            int min = 9999;
+           n =  Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[n];
+
+            for (int i = 0; i < n; i++)
+                array[i] = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i< array.Length; i++)
+                if(array[i] > max)
+                 {
+                    max = array[i];
+                   
+                }
+            for (int i = 0; i < array.Length; i++)
+                if(array[i] < min)
+                {
+                    min = array[i];
+                    
+                }
+            Console.WriteLine(max);
+            Console.WriteLine(min);
+            Console.WriteLine(max * min);
+        }
+    }
+}

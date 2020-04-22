be8ad606b8e637b5ccc82dc43911ebc3536b1316
Andrei <welldown44@gmail.com>
2020-04-22 09:23:32 +0300

    2.1 optimized
    2.1 optimized

--- Probleme LAB2/2.1/2.1/Program.cs
+++ Probleme LAB2/2.1/2.1/Program.cs
@@ -7,27 +7,28 @@ namespace _2._1
         static void Main(string[] args)
         {
             
-            int n;
-            int max = 0;
-            int min = 9999;
-           n =  Convert.ToInt32(Console.ReadLine());
-            int[] array = new int[n];
+            int numberOfElements;
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            numberOfElements =  Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[numberOfElements];
 
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < numberOfElements; i++)
                 array[i] = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = 0; i< array.Length; i++)
-                if(array[i] > max)
-                 {
-                    max = array[i];
-                   
-                }
             for (int i = 0; i < array.Length; i++)
-                if(array[i] < min)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+
+                }
+                if (array[i] < min)
                 {
                     min = array[i];
-                    
                 }
+
+            }
             Console.WriteLine(max);
             Console.WriteLine(min);
             Console.WriteLine(max * min);
